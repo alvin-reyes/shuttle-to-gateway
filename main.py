6a4a916136dc5020bc0259fcbc394d1d51430204
alvin-reyes <me@areyes.onl>
2023-03-03 10:41:59 -0500

    simple script

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import requests
 import os
 import threading
 
+
 def download_and_upload(url):
     r = requests.get(url, allow_redirects=True)
     open('./download/' + line, 'wb').write(r.content)
 
     # DOWNLOAD!!
 
-    url = "https://gateway.estuary.tech/upload"
+    url = "https://dedicated-gw.estuary.tech/upload"
     payload = {}
     files = [
         ('file', ('file', open('./download/' + line, 'rb'), 'application/octet-stream'))
     ]
     headers = {}
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    print(response.text)
+    print(response.status_code)
+
+    # remove file
+    os.remove('./download/' + line)
 
 
 with open('jin_files.txt') as jin_hashes:
     for line in jin_hashes:
         print(line, end=' ')  # The comma to suppress the extra new line char
-        url = "https://shuttle-9.estuary.tech/gw/ipfs/" + line
+        url = "https://ipfs.io/ipfs/" + line
         print(url)
 
-        # threading
-        threading.Thread(target=download_and_upload, args=(url,)).start()
+        download_and_upload(url)
+        # threading.Thread(target=download_and_upload, args=(url,)).start()
